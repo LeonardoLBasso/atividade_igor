a84e948030e56755942a70e2cce2a86704cc2f3a
Leonardo Lara Basso <leolara0811@gmail.com>
2020-09-04 21:41:11 -0300

    sixth release, switched alert for new collision mechanism

--- projeto_cachorro/js/script.js
+++ projeto_cachorro/js/script.js
@@ -1,31 +1,46 @@
+var altura = 400;
+var largura = 100;
+var pointPlayer = 0;
 
-var altura =400;
-var largura =100;
+const addPlayerPonts = () => {
+    $("#points").text(`Pontuação:  ${++pointPlayer}`);
+};
 
-$(Document).keydown(function(evento){
+// Verificando se a div tem colisão com a outra
+const hasCollision = (firstElement, secondElement) => {
+    const firstElementAxisX = firstElement.offset().left;
+    const firstElementAxisY = firstElement.offset().top;
 
-	if (largura >= 900 && altura >= 300) {
-	 alert('Atingiu o Objetivo');
-	}
-	else {
-	  if(evento.which == 37) {
-	     largura = largura - 30;
-	     $("#cachorro").css("margin-left",largura+'px');
-	  	
-	  }
-	  if(evento.which == 38) {
-	  	altura = altura - 30;
-	     $("#cachorro").css("margin-top",altura+'px');
-	  }
-	  if(evento.which == 39) {
-	  	largura = largura + 30;
-	     $("#cachorro").css("margin-left",largura+'px');
-	  	
-	  }
-	  if(evento.which == 40) {
-	  	altura = altura + 30;
-	     $("#cachorro").css("margin-top",altura+'px');
-	  	
-	  }
-	}
+    const secondElementAxisX = secondElement.offset().left;
+    const secondElementAxisY = secondElement.offset().top;
+
+    return (
+        firstElementAxisX >= secondElementAxisX - 100 && 
+        firstElementAxisX <= secondElementAxisX + 100
+    ) && (
+        firstElementAxisY >= secondElementAxisY - 100 && 
+        firstElementAxisY <= secondElementAxisY + 100
+    )
+}
+
+$(document).keydown(function (evento) {
+    if (hasCollision($("#cachorro"), $("#casa"))) {
+        addPlayerPonts();
+    }
+        if (evento.which == 37) {
+            largura = largura - 30;
+            $("#cachorro").css("margin-left", largura + "px");
+        }
+        if (evento.which == 38) {
+            altura = altura - 30;
+            $("#cachorro").css("margin-top", altura + "px");
+        }
+        if (evento.which == 39) {
+            largura = largura + 30;
+            $("#cachorro").css("margin-left", largura + "px");
+        }
+        if (evento.which == 40) {
+            altura = altura + 30;
+            $("#cachorro").css("margin-top", altura + "px");
+        }
 });
